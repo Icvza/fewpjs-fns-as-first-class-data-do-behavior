e0a54d4effc14ea7fd39f2217c704705eb18d009
Isaac <devisaacalejandro@gmail.com>
2021-05-31 22:10:09 -0400

    commit tree 80b0b39abdd45d294fa06bfbcca9e44f72dd25e0

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(time) {
+  // Just grab the hour get rid of the other half
+  const hourTime = time.split(':')
+  // This will give back an array
+  // grab the the hour and convert into a string
+  const hour = parseInt(hourTime[0])
+  // Create conditionals that will tell the time 
+  if(hour < 12 ) {
+    return "Good Morning"
+  }
+  else if(hour >= 12 && hour < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+}
